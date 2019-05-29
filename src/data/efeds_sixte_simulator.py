1ac52ed7d88d9210d8eb6e4ebf3c8d36046be546
amalyali_work <amalyali@mpe.mpg.de>
2019-05-29 16:26:55 +0200

    cfg dict for sixte simulator

--- src/data/efeds_sixte_simulator.py
+++ src/data/efeds_sixte_simulator.py
@@ -7,20 +7,14 @@ import errno
 import os
 import subprocess
 
-GTI_FILE = '../../data/raw/efeds_pv_all.gti'
-ATTITUDE = '../../data/raw/efeds_pv_all_attitude.fits'
-#SIMPUT = '../../data/raw/simput/agn/eRosita_faint_simput.fits'  # original working
-SIMPUT = '../../data/raw/simput/agn/src3_simput.fits'
-WITH_BKG_PAR = 1
-T_START = 5.7e8 # NB. t_start and exposure get overwritten by gti file
-EXPOSURE = 5.70187999e8-5.7e8
-SEED = 42
+from script_config import *
+
 
 class Simulator:
     """
     SIXTE grid simulator for transient events.
     """
-    def __init__(self, with_bkg_par, t_start, exposure, seed):
+    def __init__(self, simput, with_bkg_par, t_start, exposure, seed):
         """
         :param with_agn: Simulate with AGN.
         :param with_bkg_par: Simulate with particle background.
@@ -28,17 +22,17 @@ class Simulator:
         :param exposure: Length of time to simulate for.
         :param seed: Seed for random number generator
         """
+        self._simput = simput
         self._with_bkg_par = bool(with_bkg_par)
         self._t_start = float(t_start)  # secs
         self._exposure = float(exposure)
         self._seed = int(seed)
-        self._simput_agn = SIMPUT
 
     def make_event_directory(self):
         """
         Check for whether a directory exists and create if not.
         """
-        directory = "../../data/raw/events/efeds/"
+        directory = cfg_dict['evt_dir']
         try:
             os.makedirs(directory)
         except OSError as e:
@@ -51,11 +45,11 @@ class Simulator:
         Use this as input to SIXTE call for reducing computational expense.
         """
         cmd = ["ero_vis",
-               "Attitude=%s" % ATTITUDE,
-               "Simput=%s" % SIMPUT,
+               "Attitude=%s" % cfg_dict['attitude'],
+               "Simput=%s" % self._simput,
                "RA=0.0",
                "DEC=0.0",
-               "GTIfile=%s" % GTI_FILE,
+               "GTIfile=%s" % cfg_dict['gti_file'],
                "TSTART=%f" % self._t_start,
                "Exposure=%f" % self._exposure,
                "dt=1.0",
@@ -69,12 +63,12 @@ class Simulator:
         """
         Launch erosim from python.
         """
-        prefix = "../../data/raw/events/efeds/agn_clusters_"
+        prefix = "%s%s" % (cfg_dict['evt_dir'], cfg_dict['prefix'])
 
         cmd = ["erosim",
-               "Simput=%s" % self._simput_agn,
+               "Simput=%s" % self._simput,
                "Prefix=%s" % prefix,
-               "Attitude=%s" % ATTITUDE,
+               "Attitude=%s" % cfg_dict['attitude'],
                "RA=0.0",
                "Dec=0.0",
                "TSTART=%s" % self._t_start,
@@ -91,21 +85,21 @@ class Simulator:
         else:
             cmd.append("Background=no")
 
-        f_log = './sixte_logs/log_teng_agn_tstart_%s.txt' % self._t_start
-        with open(f_log, 'a') as log:
-            print(cmd)
-            p = subprocess.Popen(cmd, stdout=log, stderr=log)
-            p.communicate()
+        subprocess.check_call(cmd)
 
     def run_all(self):
         """
         Run SIXTE simulation of eRASS 1
         """
         self.make_event_directory()
-        #self.compute_gti()
+        self.compute_gti()
         self.run_sixte()
 
 
 if __name__ == '__main__':
-    simulator = Simulator(WITH_BKG_PAR, T_START, EXPOSURE, SEED)
+    simulator = Simulator(cfg_dict['simput'],
+                          cfg_dict['with_bkg'],
+                          cfg_dict['t_start'],
+                          cfg_dict['exposure'],
+                          cfg_dict['seed'])
     simulator.run_all()
--- src/data/script_config.py
+++ src/data/script_config.py
@@ -1,11 +1,14 @@
-cfg_dict = {"gti_file": "../../data/raw/efeds_pv_all.gti",
-            "attitude": "../../../data/raw/efeds_pv_all_attitude.fits",
-            "simput": "../../data/raw/simput/",
+cfg_dict = {"simput": "../../data/raw/simput/",
             "n_src": 100,
             "simput_e_min": 0.5,
             "simput_e_max": 2.0,
             "simput_spec_n_bins": 1000,
             "t_start": 5.7e8,
             "exposure": 0.00187999e8,
-            "seed": 42
+            "seed": 42,
+            "with_bkg": True,
+            "gti_file": "../../data/raw/efeds_pv_all.gti",
+            "attitude": "../../../erosita_efeds/data/raw/efeds_pv_all_attitude.fits",
+            "evt_dir": "../../data/raw/events/efeds/",
+            "prefix": "blank_"
             }
